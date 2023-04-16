2a27138d978c5a05133899f52ccda8114df82af0
Mohammed Al-Mahdi <masaylighto@gmail.com>
2023-04-16 09:32:27 +0300

    Implement the ability to receive gpt answer as streamed http response

--- Logic/Repositories/GPTClient.cs
+++ Logic/Repositories/GPTClient.cs
@@ -4,6 +4,8 @@ using Logic.Core.Models;
 using OneOf;
 using Polly;
 using Polly.Retry;
+using System;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
@@ -20,9 +22,16 @@ namespace Logic.Repositories
         {
             ApiConfig = apiConfig;
             Logger = logger;
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiConfig.GPTApiToken);
             retryPolicy = CreateRetryPolicy();
-        } 
+            ApplyRequestHeaders();
+        }
+        void ApplyRequestHeaders() 
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ApiConfig.GPTApiToken);
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("zip"));
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+            client.DefaultRequestHeaders.Connection.Add("keep-alive");
+        }
         private AsyncRetryPolicy<HttpResponseMessage> CreateRetryPolicy()
         {
 
@@ -41,21 +50,76 @@ namespace Logic.Repositories
 
             return await retryPolicy.ExecuteAsync(HttpRequestFunc);
         }
-
+      
+        HttpRequestMessage BuildRequestMessage(string Question)
+        {
+            var messageContent = BuildRequestBody(Question);
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, ApiConfig.ApiEndPoint);
+            httpRequestMessage.Content = messageContent;
+            return httpRequestMessage;
+        }
         public async Task<OneOf<String, Exception>> GetAnswer(string Question)
         {
             try
             {
-                var messageContent = BuildRequestBody(Question);
-                var gptResponse = await ExcuteWithPolicy(async () => await client.PostAsync(ApiConfig.ApiEndPoint, messageContent));
-                var response = await gptResponse.Content.ReadAsStringAsync();
+                //depend on if we want to receive the HTTP response streamed or EndtoEnd
+                var CompletionOption = ApiConfig.Stream ? HttpCompletionOption.ResponseHeadersRead:
+                                                          HttpCompletionOption.ResponseContentRead;
+                var gptResponse = await ExcuteWithPolicy(async () => await client.SendAsync(BuildRequestMessage(Question), CompletionOption));
+                gptResponse.EnsureSuccessStatusCode();
                 if (!gptResponse.IsSuccessStatusCode)
+                {
+                    return new HttpRequestException(await gptResponse.Content.ReadAsStringAsync());
+                }
+                //GPT offer two way to response; a streamed response where it send the answer piece by piece
+                //and an EndtoEnd where the result send as one piece,
+                //we can define which one by including "stream(boolean)" parameter in our request
+                //we here handle both cases 
+                return ApiConfig.Stream ? await GetAnswerStreamed(gptResponse):
+                                          await GetAnswerEndToEnd(gptResponse);
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+    
+        }
+   
+        async Task<OneOf<String, Exception>> GetAnswerStreamed(HttpResponseMessage responseMessage)
+        {
+            try
+            {
+                //get steam get lines convert them to json combine answer return it               
+                string answer = string.Empty;
+                foreach (var response in ExtractAnswers(ResponseToLines(responseMessage)))
+                {                   
+                   answer += response;                  
+                } 
+                return answer;
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+
+}
+        async Task<OneOf<String, Exception>> GetAnswerEndToEnd(HttpResponseMessage responseMessage)
+        {
+            try
+            {
+                var response = await responseMessage.Content.ReadAsStringAsync();
+                if (!responseMessage.IsSuccessStatusCode)
                 {
                     return new HttpRequestException(response);
                 }
                 var deserializeContent = JsonSerializer.Deserialize<GptResponse>(response);
-                if (deserializeContent is null || deserializeContent.choices is null || deserializeContent.choices.Length == 0)
-                {
+                if (deserializeContent is null ||
+                     deserializeContent.choices is null ||
+                     deserializeContent.choices.Length == 0 ||
+                     deserializeContent.choices.First().message is null ||
+                     string.IsNullOrEmpty(deserializeContent.choices.First().message.content)) 
+                { 
+
                     return new Exception("No Answer in Response");
                 }
                 return deserializeContent.choices.First().message.content;
@@ -64,7 +128,55 @@ namespace Logic.Repositories
             {
                 return ex;
             }
-    
+       
+
+        }
+        IEnumerable<string> ResponseToLines(HttpResponseMessage stream)
+        {
+            var responseStream = stream.Content.ReadAsStream();
+            var streamReader = new StreamReader(responseStream);
+            while (!streamReader.EndOfStream)
+            {
+                var response = streamReader.ReadLine();
+                if (string.IsNullOrEmpty(response))
+                {
+                    continue;
+                }
+                yield return response;
+            }
+        }
+        IEnumerable<string> ExtractAnswers(IEnumerable<string> responses)
+        {
+            foreach (var response in responses)
+            {
+                GptResponse deserializeContent = default;
+                try // not the best way out there to handle bad json, but its temporary 
+                {
+                    var indexOfjsonStart= response.IndexOf("{");
+                    if (indexOfjsonStart==-1)
+                    {
+                        continue;
+                    }
+                    var jsonString=response.Substring(indexOfjsonStart, response.Length- indexOfjsonStart);// remove anything before json brackets
+                    deserializeContent = JsonSerializer.Deserialize<GptResponse>(jsonString);
+                }
+                catch
+                {
+
+                }
+
+                if (deserializeContent is null ||
+                    deserializeContent.choices is null ||
+                    deserializeContent.choices.Length == 0 ||
+                    deserializeContent.choices.First().delta is null ||
+                    string.IsNullOrEmpty(deserializeContent.choices.First().delta.content))
+                {
+                    continue;
+                }
+                yield return deserializeContent.choices.First().delta.content;
+
+            }
+
         }
         public StringContent BuildRequestBody(string Question)
         {
@@ -82,6 +194,7 @@ namespace Logic.Repositories
                 top_p = ApiConfig.NucleusSampling,
                 n = ApiConfig.ChatCompletionChoicesForEachMessage,
                 stop = ApiConfig.FurtherTokens,
+                stream=ApiConfig.Stream
             });
             return new StringContent(serializedMessage, MediaTypeHeaderValue.Parse("application/json"));
         }
