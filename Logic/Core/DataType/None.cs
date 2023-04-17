87011096f4ccfeeca89ab3588cdb056b6ebdceac
Mohammed Al-Mahdi <masaylighto@gmail.com>
2023-04-17 10:15:12 +0300

    Optimize GPT Client && remove polly (no need for it + take to much time)

---
+++ Logic/Core/DataType/None.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Logic.Core.DataType;
+
+/// <summary>
+/// to be used as indicator if method completed without return (useful with OneOf,AnyOf or any similar lib)
+/// </summary>
+public record None();
